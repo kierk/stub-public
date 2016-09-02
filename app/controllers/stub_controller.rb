9e5b9cbf3d61ad2b5c9543cc68c12fd20f5b4ba2
Andrew Pellitieri <apellitieri@mitre.org>
2016-09-02 10:21:06 -0400

    Made some initial testing edits

---
+++ app/controllers/stub_controller.rb
@@ -0,0 +1,16 @@
+class StubController < ApplicationController
+    # before_action :set_stub, only: [:show]
+
+    # GET /info
+    # GET /info.json
+    def index
+    end
+end
+    # private
+    #     def set_stub
+    #         @stub = Stub.find(params[:id])
+    #     end
+
+        # def stub_params
+        #     params.require(:task).permit(:title, :note)
+        # end
